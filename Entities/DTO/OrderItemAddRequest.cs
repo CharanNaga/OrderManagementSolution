2db7c3057a72c35f80a74b927975f4d344ebe454
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-03 15:20:50 +0530

    Created OrderItemAddRequest, OrderItemResponse & OrderItemUpdateRequest classes handling entity class OrderItem

---
+++ Entities/DTO/OrderItemAddRequest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTO
+{
+    public class OrderItemAddRequest
+    {
+        public Guid OrderID { get; set; }
+
+        [Required(ErrorMessage = "Product Name can't be blank")]
+        [StringLength(50, ErrorMessage = "Product Name must not exceed 50 characters")]
+        public string? ProductName { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be a positive number.")]
+        public int Quantity { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "Unit Price must be a positive number.")]
+        [Column(TypeName = "decimal")]
+        public decimal UnitPrice { get; set; }
+
+        [Range(0, double.MaxValue)]
+        [Column(TypeName = "decimal")]
+        public decimal TotalPrice { get; set; }
+
+        public OrderItem ToOrderItem()
+        {
+            return new OrderItem()
+            { 
+                OrderID = OrderID,
+                ProductName = ProductName,
+                Quantity = Quantity,
+                UnitPrice = UnitPrice,
+                TotalPrice = TotalPrice,
+            };
+        }
+    }
+}
---
+++ Entities/DTO/OrderItemResponse.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTO
+{
+    public class OrderItemResponse
+    {
+        public Guid OrderItemID { get; set; }
+
+        public Guid OrderID { get; set; }
+
+        public string? ProductName { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+    public static class OrderItemExtensions
+    {
+        public static OrderItemResponse ToOrderItemResponse(this OrderItem orderItem)
+        {
+            return new OrderItemResponse()
+            {
+                OrderItemID = orderItem.OrderItemID,
+                OrderID = orderItem.OrderID,
+                ProductName = orderItem.ProductName,
+                Quantity = orderItem.Quantity,
+                UnitPrice = orderItem.UnitPrice,
+                TotalPrice = orderItem.TotalPrice,
+            };
+        }
+    }
+}
