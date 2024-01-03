5440c534855606ff0dfdbd0d1d0970a52458b169
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-03 18:35:05 +0530

    Created Interfaces under Orders folder for Adding, Updating, Filtering, Retrieving & Deleting Functions

---
+++ ServiceContracts/Orders/IOrdersGetterService.cs
@@ -0,0 +1,15 @@
+﻿using ServiceContracts.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceContracts.Orders
+{
+    public interface IOrdersGetterService
+    {
+        Task<List<OrderResponse>> GetAllOrders();
+        Task<OrderResponse>? GetOrderByOrderID(Guid orderID);
+    }
+}
