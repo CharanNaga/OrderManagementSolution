b1e73c74bc3b75ee6e12f9430151a5fddd786dcc
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-03 12:54:52 +0530

    Added Data Annotations for the Entity Model Classes

--- Entities/Order.cs
+++ Entities/Order.cs
@@ -1,11 +1,29 @@
-﻿namespace Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities
 {
     public class Order
     {
+        [Key]
         public Guid OrderID { get; set; }
+
+        [Required(ErrorMessage ="Order Number can't be blank")]
+        [RegularExpression(@"^(?i)ORD_\d{4}_\d+$\r\n", ErrorMessage = "Order Number should begin with 'ORD' followed by an underscore (_) and a sequential number.")] 
         public string? OrderNumber { get; set; }
+
+        [Required(ErrorMessage ="Customer Name can't be blank")]
+        [StringLength(50,ErrorMessage ="Customer Name must not exceed 50 characters")]
         public string? CustomerName { get; set;}
+
+
+        [Required(ErrorMessage ="Order Date can't be blank")]
         public DateTime OrderDate { get; set; }
+
+        [Range(0,double.MaxValue,ErrorMessage ="Total Amount must be positive")]
+        [Column(TypeName = "decimal")]
         public decimal TotalAmount { get; set; }
+
+        public virtual ICollection<OrderItem>? OrderItems { get; set; }
     }
 }
--- Entities/OrderItem.cs
+++ Entities/OrderItem.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Buffers.Text;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -11,11 +13,28 @@ namespace Entities
 {
     public class OrderItem
     {
+        [Key]
         public Guid OrderItemID { get; set; }
+
+        [Required(ErrorMessage ="Order ID can't be blank")]
         public Guid OrderID { get; set; }
+
+        [Required(ErrorMessage = "Product Name can't be blank")]
+        [StringLength(50, ErrorMessage = "Product Name must not exceed 50 characters")]
         public string? ProductName { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be a positive number.")]
         public int Quantity { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "Unit Price must be a positive number.")]
+        [Column(TypeName = "decimal")]
         public decimal UnitPrice { get; set; }
+
+        [Range(0, double.MaxValue)]
+        [Column(TypeName = "decimal")]
         public decimal TotalPrice { get; set; }
+
+        [ForeignKey(nameof(OrderID))]
+        public virtual Order? Order { get; set; }
     }
 }
