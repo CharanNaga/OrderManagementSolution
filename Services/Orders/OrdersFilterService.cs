fb7bb2fb8092807197e76d16a9900e4db37283e2
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-03 22:20:59 +0530

    Created OrdersFilterService.cs in Services/Orders & injected required Services, & implemented dummy implementation

---
+++ Services/Orders/OrdersFilterService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using RepositoryContracts;
+using ServiceContracts.DTO;
+using ServiceContracts.Orders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Orders
+{
+    public class OrdersFilterService : IOrdersFilterService
+    {
+        private readonly IOrdersRepository _ordersRepository;
+        private readonly ILogger<OrdersFilterService> _logger;
+
+        public OrdersFilterService(IOrdersRepository ordersRepository, ILogger<OrdersFilterService> logger)
+        {
+            _ordersRepository = ordersRepository;
+            _logger = logger;
+        }
+
+        public Task<List<OrderResponse>> GetFilteredOrders(string searchBy, string? searchString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
