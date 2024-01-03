1a656772a20dd16db45e99970b03bdf738798940
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-03 23:17:19 +0530

    Implemented DeleteOrderItemByOrderItemID() with actual logic in OrderItemsDeleterService.cs

--- Services/OrderItems/OrderItemsDeleterService.cs
+++ Services/OrderItems/OrderItemsDeleterService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Entities;
+using Microsoft.Extensions.Logging;
 using RepositoryContracts;
 using ServiceContracts.OrderItems;
 using System;
@@ -20,9 +21,26 @@ namespace Services.OrderItems
             _logger = logger;
         }
 
-        public Task<bool> DeleteOrderItemByOrderItemID(Guid orderItemID)
+        public async Task<bool> DeleteOrderItemByOrderItemID(Guid orderItemID)
         {
-            throw new NotImplementedException();
+            _logger.LogInformation("DeleteOrderItemByOrderItemID Service starts");
+
+            //1. Delete matching object object by calling corresponding Repository Method
+            var isDeleted = await _orderItemsRepository.DeleteOrderItemByOrderItemID(orderItemID);
+
+            if (isDeleted)
+            {
+                _logger.LogInformation($"Order Item with OrderItemID: {orderItemID} deleted successfully");
+            }
+            else
+            {
+                _logger.LogWarning($"Order Item with OrderItemID: {orderItemID} not found");
+            }
+
+            _logger.LogInformation("DeleteOrderItemByOrderItemID Service ends");
+
+            //2. Return boolean value indicating orderitem object was deleted or not
+            return isDeleted;
         }
     }
 }
