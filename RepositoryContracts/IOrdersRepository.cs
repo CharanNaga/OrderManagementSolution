9bb914f64f4aca4f94c56f9b30a4327d2be353c9
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-03 19:26:49 +0530

    Created IOrdersRepository for writing methods consisted to perform CRUD Operations on direct Entity Order

---
+++ RepositoryContracts/IOrdersRepository.cs
@@ -0,0 +1,16 @@
+﻿using Entities;
+using System.Linq.Expressions;
+
+namespace RepositoryContracts
+{
+    public interface IOrdersRepository
+    {
+        Task<Order> AddOrder(Order order);
+        Task<List<Order>> GetAllOrders();
+        Task<Order>? GetOrderByOrderID(Guid orderID);
+        Task<List<Order>> GetFilteredOrders(Expression<Func<Order, bool>> predicate);
+        Task<Order> UpdateOrder(Order order);
+        Task<bool> DeleteOrderByOrderID(Guid orderID);
+
+    }
+}
