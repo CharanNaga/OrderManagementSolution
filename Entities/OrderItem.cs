a69d9af651ae4b977cc00ffb8cf3dbbf3fd39b5f
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-03 12:23:45 +0530

    Created OrderItem.cs with some properties in Entities Project

---
+++ Entities/OrderItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Entities
+{
+    public class OrderItem
+    {
+        public Guid OrderItemID { get; set; }
+        public Guid OrderID { get; set; }
+        public string? ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
