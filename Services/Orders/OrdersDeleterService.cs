a1696f06a201483aa5169ee9b80cad5359297927
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-03 22:08:36 +0530

    Created OrdersDeleterService.cs in Services/Orders & injected required Services, & implemented dummy implementation

---
+++ Services/Orders/OrdersDeleterService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using RepositoryContracts;
+using ServiceContracts.Orders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Orders
+{
+    public class OrdersDeleterService : IOrdersDeleterService
+    {
+        private readonly IOrdersRepository _ordersRepository;
+        private readonly ILogger<OrdersDeleterService> _logger;
+
+        public OrdersDeleterService(IOrdersRepository ordersRepository, ILogger<OrdersDeleterService> logger)
+        {
+            _ordersRepository = ordersRepository;
+            _logger = logger;
+        }
+
+        public Task<bool> DeleteOrderByOrderID(Guid orderID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
