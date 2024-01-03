bb1469e5b13d0e1df7b19356eea12793c8859709
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-04 01:06:42 +0530

    Created OrderItemsController.cs & Injected required services

---
+++ OrderManagement.WebAPI/Controllers/OrderItemsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServiceContracts.OrderItems;
+
+namespace OrderManagement.WebAPI.Controllers
+{
+    [Route("api/orders/{orderID}/items")]
+    [ApiController]
+    public class OrderItemsController : ControllerBase
+    {
+        private readonly IOrderItemsGetterService _orderItemsGetterService;
+        private readonly IOrderItemsAdderService _orderItemsAdderService;
+        private readonly IOrderItemsUpdaterService _orderItemsUpdaterService;
+        private readonly IOrderItemsDeleterService _orderItemsDeleterService;
+        private readonly ILogger<OrderItemsController> _logger;
+
+        public OrderItemsController(IOrderItemsGetterService orderItemsGetterService,IOrderItemsAdderService orderItemsAdderService,IOrderItemsUpdaterService orderItemsUpdaterService,IOrderItemsDeleterService orderItemsDeleterService,ILogger<OrderItemsController> logger)
+        {
+            _orderItemsGetterService = orderItemsGetterService;
+            _orderItemsAdderService = orderItemsAdderService;
+            _orderItemsUpdaterService = orderItemsUpdaterService;
+            _orderItemsDeleterService = orderItemsDeleterService;
+            _logger = logger;
+        }
+
+
+    }
+}
