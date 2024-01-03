dc4838a3fe87e69b50df92943b6b83aa268bc447
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-03 20:27:14 +0530

    Created OrdersRepository & injected DbContext, ILogger Services and implemented interface by providing dummy implementation

---
+++ Repositories/OrdersRepository.cs
@@ -0,0 +1,54 @@
+﻿using Entities;
+using Microsoft.Extensions.Logging;
+using RepositoryContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class OrdersRepository : IOrdersRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly ILogger<OrdersRepository> _logger;
+
+        public OrdersRepository(ApplicationDbContext db, ILogger<OrdersRepository> logger)
+        {
+            _db = db;
+            _logger = logger;
+        }
+
+        public Task<Order> AddOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteOrderByOrderID(Guid orderID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Order>> GetAllOrders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Order>> GetFilteredOrders(Expression<Func<Order, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Order>? GetOrderByOrderID(Guid orderID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Order> UpdateOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
