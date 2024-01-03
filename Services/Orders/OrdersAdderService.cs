83c38fff321e7c2c78237799bc3d37339a67b304
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-03 21:14:24 +0530

    Created OrdersAdderService.cs in Services/Orders & injected required Services, & implemented dummy implementation

---
+++ Services/Orders/OrdersAdderService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using RepositoryContracts;
+using ServiceContracts.DTO;
+using ServiceContracts.Orders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Orders
+{
+    public class OrdersAdderService : IOrdersAdderService
+    {
+        private readonly IOrdersRepository _ordersRepository;
+        private readonly IOrderItemsRepository _orderItemsRepository;
+        private readonly ILogger<OrdersAdderService> _logger;
+
+        public OrdersAdderService(IOrdersRepository ordersRepository, IOrderItemsRepository orderItemsRepository, ILogger<OrdersAdderService> logger)
+        {
+            _ordersRepository = ordersRepository;
+            _orderItemsRepository = orderItemsRepository;
+            _logger = logger;
+        }
+
+        public Task<OrderResponse> AddOrder(OrderAddRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
