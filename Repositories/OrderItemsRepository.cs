d0b18a2a23fadb2760aac2a71020b1bf37c5206e
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-03 19:38:27 +0530

    Created OrderItemsRepository & injected DbContext, ILogger Services and implemented interface by providing dummy implementation

---
+++ Repositories/OrderItemsRepository.cs
@@ -0,0 +1,53 @@
+﻿using Entities;
+using Microsoft.Extensions.Logging;
+using RepositoryContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class OrderItemsRepository : IOrderItemsRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly ILogger<OrderItemsRepository> _logger;
+
+        public OrderItemsRepository(ApplicationDbContext db, ILogger<OrderItemsRepository> logger)
+        {
+            _db = db;
+            _logger = logger;
+        }
+
+        public Task<OrderItem> AddOrderItem(OrderItem orderItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteOrderItemByOrderItemID(Guid orderItemID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<OrderItem>> GetAllOrderItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OrderItem>? GetOrderItemByOrderItemID(Guid orderItemID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<OrderItem>> GetOrderItemsByOrderID(Guid orderID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OrderItem> UpdateOrderItem(OrderItem orderItem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
