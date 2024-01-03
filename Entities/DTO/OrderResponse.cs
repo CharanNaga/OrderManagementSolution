ae89dc75119eef72ad9a849ef58450d2ac403c66
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-03 15:20:14 +0530

    Created OrderAddRequest, OrderResponse & OrderUpdateRequest classes handling entity class Order

---
+++ Entities/DTO/OrderResponse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTO
+{
+    public class OrderResponse
+    {
+        public Guid OrderID { get; set; }
+        public string? OrderNumber { get; set; }
+        public string? CustomerName { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+    public static class OrderExtensions
+    {
+        public static OrderResponse ToOrderResponse(this Order order)
+        {
+            return new OrderResponse()
+            {
+                OrderID = order.OrderID,
+                OrderNumber = order.OrderNumber,
+                CustomerName = order.CustomerName,
+                OrderDate = order.OrderDate,
+                TotalAmount = order.TotalAmount
+            };
+        }
+    }
+}
---
+++ Entities/DTO/OrderUpdateRequest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTO
+{
+    public class OrderUpdateRequest
+    {
+        public Guid OrderID { get; set; }
+
+        [Required(ErrorMessage = "Order Number can't be blank")]
+        [RegularExpression(@"^(?i)ORD_\d{4}_\d+$\r\n", ErrorMessage = "Order Number should begin with 'ORD' followed by an underscore (_) and a sequential number.")]
+        public string? OrderNumber { get; set; }
+
+        [Required(ErrorMessage = "Customer Name can't be blank")]
+        [StringLength(50, ErrorMessage = "Customer Name must not exceed 50 characters")]
+        public string? CustomerName { get; set; }
+
+
+        [Required(ErrorMessage = "Order Date can't be blank")]
+        public DateTime OrderDate { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "Total Amount must be positive")]
+        [Column(TypeName = "decimal")]
+        public decimal TotalAmount { get; set; }
+
+        public Order ToOrder()
+        {
+            return new Order()
+            {
+                OrderID = OrderID,
+                OrderNumber = OrderNumber,
+                CustomerName = CustomerName,
+                OrderDate = OrderDate,
+                TotalAmount = TotalAmount
+            };
+        }
+    }
+}
