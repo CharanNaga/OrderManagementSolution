318f00fdf386c6b25bed78f99f19c66d67490c48
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-04 00:29:00 +0530

    Added Repository Classes & Service Classes as the services to the Program.cs

--- OrderManagement.WebAPI/Program.cs
+++ OrderManagement.WebAPI/Program.cs
@@ -1,5 +1,11 @@
 using Entities;
 using Microsoft.EntityFrameworkCore;
+using Repositories;
+using RepositoryContracts;
+using ServiceContracts.OrderItems;
+using ServiceContracts.Orders;
+using Services.OrderItems;
+using Services.Orders;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +19,20 @@ builder.Services.AddDbContext<ApplicationDbContext>(
     {
         options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
     });
+
+//Add custom services
+builder.Services.AddScoped<IOrdersRepository, OrdersRepository>();
+builder.Services.AddScoped<IOrderItemsRepository, OrderItemsRepository>();
+builder.Services.AddScoped<IOrdersAdderService, OrdersAdderService>();
+builder.Services.AddScoped<IOrdersDeleterService, OrdersDeleterService>();
+builder.Services.AddScoped<IOrdersFilterService, OrdersFilterService>();
+builder.Services.AddScoped<IOrdersGetterService, OrdersGetterService>();
+builder.Services.AddScoped<IOrdersUpdaterService, OrdersUpdaterService>();
+builder.Services.AddScoped<IOrderItemsAdderService, OrderItemsAdderService>();
+builder.Services.AddScoped<IOrderItemsDeleterService, OrderItemsDeleterService>();
+builder.Services.AddScoped<IOrderItemsGetterService, OrderItemsGetterService>();
+builder.Services.AddScoped<IOrderItemsUpdaterService, OrderItemsUpdaterService>();
+
 
 var app = builder.Build();
 
