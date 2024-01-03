24c24d1923db9b220bbe2627363c05e5ec9178dd
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-03 23:27:42 +0530

    Created OrderItemsGetterService.cs in Services/OrderItems & injected required Services, & implemented dummy implementation

---
+++ Services/OrderItems/OrderItemsGetterService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Logging;
+using RepositoryContracts;
+using ServiceContracts.DTO;
+using ServiceContracts.OrderItems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.OrderItems
+{
+    public class OrderItemsGetterService : IOrderItemsGetterService
+    {
+        private readonly IOrderItemsRepository _orderItemsRepository;
+        private readonly ILogger<OrderItemsGetterService> _logger;
+
+        public OrderItemsGetterService(IOrderItemsRepository orderItemsRepository, ILogger<OrderItemsGetterService> logger)
+        {
+            _orderItemsRepository = orderItemsRepository;
+            _logger = logger;
+        }
+
+        public Task<List<OrderItemResponse>> GetAllOrderItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OrderItemResponse>? GetOrderItemByOrderItemID(Guid orderItemID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<OrderItemResponse>> GetOrderItemsByOrderID(Guid orderID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
